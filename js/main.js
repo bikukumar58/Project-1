32d45cc71aa9d236b14a839ab4aa9694bc1a5f09
bikukumar58 <bikukumar58@gmail.com>
2024-08-07 16:22:02 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+  $(document).ready(function(){
+
+    $('.responsive').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed:2000,
+      prevArrow:
+      '<i class="bi bi-caret-right-fill slick-prev">',
+     nextArrow:
+      '<i class="bi bi-caret-right-fill slick-next">',
+    });
+
+  });
+
